f21f9dcdff8f9477b596a25cf4ef99d3948f50f7
Chun Yang <canadachunyang@gmail.com>
2018-02-01 15:26:23 +0000

    new

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-module.exports = 'blahblahblah'
+module.exports = {
+  function (x){
+    let y = x.split(' ').join().split('');
+    for(let i=0; i < y.length; i+2){
+      if(i >= y.length){
+        return y.join();
+      } else {
+        y[i].toUpperCase();
+      }
+    }
+  }
+}
